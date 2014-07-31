670213feeb7f1dae76c857f6eaa6d89369ec098e
Daniel Bower <daniel.d.bower@gmail.com>
2014-07-30 22:46:11 -0400

    Feature - Turn up logging for grails.app and com.infinum to debug - create test data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,24 @@
-class BootStrap {
+import org.apache.commons.lang.RandomStringUtils
 
+import com.infinum.txexamples.Datum
+
+class BootStrap {
+	private static final int randomStringLength = 16
+	private static final String charset = (('a'..'z') + ('A'..'Z') + ('0'..'9')).join()
+	
     def init = { servletContext ->
+		if(Datum.count() == 0){
+			(0..50000).each{
+				String randomString = RandomStringUtils.random(randomStringLength, charset.toCharArray())
+				if(!new Datum(randValue:randomString).save()){
+					log.error("Failed to save Datum")
+				}
+				
+				if((it%1000) == 0){
+					log.info "Inserted Datum $it"
+				}
+			}
+		}
     }
     def destroy = {
     }
---
+++ grails-app/domain/com/infinum/txexamples/Datum.groovy
@@ -0,0 +1,10 @@
+package com.infinum.txexamples
+
+/**
+ * This is the class that we'll try to deadlock
+ *
+ */
+class Datum {
+
+	String randValue
+}
